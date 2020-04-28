3bfe340c70687ac22844b6a7d142ee2352b5c36e
Anjumzaki <anjumzaki8@gmail.com>
2020-04-29 01:16:20 +0500

    checkout screen done

--- src/screens/SignUp1.js
+++ src/screens/SignUp1.js
@@ -9,21 +9,22 @@ import {
   ScrollView,
   TouchableOpacity,
   StatusBar,
-  Dimensions
+  Dimensions,
 } from "react-native";
 import { BackStack } from "../Helpers/BackStack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import Modal from 'react-native-modalbox';
-import CodeInput from 'react-native-confirmation-code-input';
+import Modal from "react-native-modalbox";
+import CodeInput from "react-native-confirmation-code-input";
 import * as Font from "expo-font";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
   listenOrientationChangeText as lor,
-  removeOrientationListener as rol
+  removeOrientationListener as rol,
 } from "react-native-responsive-screen";
 import { conStyles, textStyles, textIn, btnStyles } from "../styles/base";
+
 import LatoText from "../Helpers/LatoText";
 import axios from "axios";
 
@@ -49,19 +50,19 @@ export default class SignUp1 extends React.Component {
       sliderValue: 0.3,
       codeMsg: false,
       numVerified: false,
-      num: ""
+      num: "",
     };
   }
   onClose() {
-    console.log('Modal just closed');
+    console.log("Modal just closed");
   }
 
   onOpen() {
-    console.log('Modal just opened');
+    console.log("Modal just opened");
   }
 
   onClosingState(state) {
-    console.log('the open/close of the swipeToClose just changed');
+    console.log("the open/close of the swipeToClose just changed");
   }
   async componentDidMount() {
     await Font.loadAsync({
@@ -70,12 +71,12 @@ export default class SignUp1 extends React.Component {
       "Lato-Regular": require("../../assets/fonts/Lato-Regular.ttf"),
       "Sarabun-Regular": require("../../assets/fonts/Sarabun-Regular.ttf"),
       "Sarabun-Medium": require("../../assets/fonts/Sarabun-Medium.ttf"),
-      "Sarabun-Light": require("../../assets/fonts/Sarabun-Light.ttf")
+      "Sarabun-Light": require("../../assets/fonts/Sarabun-Light.ttf"),
     });
 
     this.setState({ fontLoaded: true });
   }
-  getRef = ref => {
+  getRef = (ref) => {
     if (this.props.getRef) this.props.getRef(ref);
   };
   changePwdType = () => {
@@ -83,21 +84,27 @@ export default class SignUp1 extends React.Component {
     // set new state value
     this.setState({
       icEye: isPassword ? "visibility" : "visibility-off",
-      isPassword: !isPassword
+      isPassword: !isPassword,
     });
   };
 
   render() {
     const { icEye, isPassword } = this.state;
 
-    console.log("this", this.state)
+    console.log("this", this.state);
     // console.log();
     // console.log("num", num)
     return (
-      <SafeAreaView style={[conStyles.safeAreaMy, { backgroundColor: 'white' }]}>
+      <SafeAreaView
+        style={[conStyles.safeAreaMy, { backgroundColor: "white" }]}
+      >
         <StatusBar translucent={true} barStyle="dark-content" />
-        <Modal style={[styles.modal, styles.modal3]} position={"center"} ref={"modal3"} isDisabled={this.state.isDisabled}>
-
+        <Modal
+          style={[styles.modal, styles.modal3]}
+          position={"center"}
+          ref={"modal3"}
+          isDisabled={this.state.isDisabled}
+        >
           <LatoText
             fontName="Lato-Regular"
             fonSiz={20}
@@ -109,54 +116,71 @@ export default class SignUp1 extends React.Component {
             fontName="Lato-Regular"
             fonSiz={15}
             col="#5C5C5C"
-            txtAlign={'center'}
+            txtAlign={"center"}
             text={"A 6-digit code has been sent to example@gmail.com"}
           />
           {this.state.num.length > 0 ? (
-          <CodeInput
-            ref="codeInputRef2"
-            // compareWithCode={this.state.num}
-            compareWithCode= {this.state.num}
-            activeColor='#000000'
-            inactiveColor='#000000'
-            autoFocus={true}
-            ignoreCase={true}
-            codeLength={6}
-            inputPosition='center'
-            size={wp(8)}
-            onFulfill={(isValid) => isValid ? this.setState({ codeMsg: false, numVerified: true }, this.refs.modal3.close()) : this.setState({ codeMsg: true })}
-            containerStyle={{ marginTop: 30 }}
-            codeInputStyle={{ borderWidth: 1.5, borderRadius: 5, borderColor: '#EFEFF4', color: '#000000' }}
-          />
-          ): null}
-          {this.state.codeMsg && <LatoText
-            fontName="Lato-Regular"
-            fonSiz={15}
-            col="red"
-            txtAlign={'center'}
-            text={"Code is incorect"}
-          />}
+            <CodeInput
+              ref="codeInputRef2"
+              // compareWithCode={this.state.num}
+              compareWithCode={this.state.num}
+              activeColor="#000000"
+              inactiveColor="#000000"
+              autoFocus={true}
+              ignoreCase={true}
+              codeLength={6}
+              inputPosition="center"
+              size={wp(8)}
+              onFulfill={(isValid) =>
+                isValid
+                  ? this.setState(
+                      { codeMsg: false, numVerified: true },
+                      this.refs.modal3.close()
+                    )
+                  : this.setState({ codeMsg: true })
+              }
+              containerStyle={{ marginTop: 30 }}
+              codeInputStyle={{
+                borderWidth: 1.5,
+                borderRadius: 5,
+                borderColor: "#EFEFF4",
+                color: "#000000",
+              }}
+            />
+          ) : null}
+          {this.state.codeMsg && (
+            <LatoText
+              fontName="Lato-Regular"
+              fonSiz={15}
+              col="red"
+              txtAlign={"center"}
+              text={"Code is incorect"}
+            />
+          )}
           <View style={{ paddingBottom: 10 }} />
           <TouchableOpacity onPress={() => this.refs.modal3.close()}>
-
             <LatoText
               fontName="Lato-Regular"
               fonSiz={15}
               col="#B50000"
-              txtAlign={'center'}
+              txtAlign={"center"}
               text={"Cancel"}
             />
           </TouchableOpacity>
-
         </Modal>
-        <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{ paddingLeft: 30, paddingTop: 30, paddingBottom: 10 }}>
-          <Image source={require('../../assets/back.png')} />
+        <TouchableOpacity
+          onPress={() => this.props.navigation.goBack()}
+          style={{ paddingLeft: 30, paddingTop: 30, paddingBottom: 10 }}
+        >
+          <Image source={require("../../assets/back.png")} />
         </TouchableOpacity>
         <ScrollView
           style={{ flex: 1 }}
-          contentContainerStyle={[conStyles.scroll, { justifyContent: 'space-around' }]}
+          contentContainerStyle={[
+            conStyles.scroll,
+            { justifyContent: "space-around" },
+          ]}
         >
-
           <Image
             style={styles.logo}
             source={require(".././../assets/logo.png")}
@@ -184,11 +208,15 @@ export default class SignUp1 extends React.Component {
                 />
               </View>
               <View>
-                <TextInput style={textIn.input}
-                  onChangeText={(name) => this.setState({
-                    name
-                  })}
-                  value={this.state.name} />
+                <TextInput
+                  style={textIn.input}
+                  onChangeText={(name) =>
+                    this.setState({
+                      name,
+                    })
+                  }
+                  value={this.state.name}
+                />
               </View>
             </View>
             <View>
@@ -201,15 +229,16 @@ export default class SignUp1 extends React.Component {
                 />
               </View>
               <View>
-
-                <TextInput style={textIn.input}
-                  onChangeText={(email) => this.setState({
-                    email
-                  })}
-                  value={this.state.email} />
-
+                <TextInput
+                  style={textIn.input}
+                  onChangeText={(email) =>
+                    this.setState({
+                      email,
+                    })
+                  }
+                  value={this.state.email}
+                />
               </View>
-
             </View>
             <View>
               <View style={textIn.label}>
@@ -220,13 +249,25 @@ export default class SignUp1 extends React.Component {
                   text={"Phone Number"}
                 />
               </View>
-              <View style={{ marginBottom: 10, flexDirection: 'row', alignContent: 'center', alignItems: 'center' }}>
+              <View
+                style={{
+                  marginBottom: 10,
+                  flexDirection: "row",
+                  alignContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 <Image
-                  style={{ width: wp('8%') }}
-                  source={require('../../assets/america.png')}
+                  style={{ width: wp("8%") }}
+                  source={require("../../assets/america.png")}
                 />
                 <View
-                  style={{ width: wp('8%'), justifyContent: 'center', alignContent: 'center', paddingLeft: 5 }}
+                  style={{
+                    width: wp("8%"),
+                    justifyContent: "center",
+                    alignContent: "center",
+                    paddingLeft: 5,
+                  }}
                 >
                   <LatoText
                     fontName="Lato-Regular"
@@ -234,16 +275,18 @@ export default class SignUp1 extends React.Component {
                     col="#5C5C5C"
                     text={"+1"}
                   />
-
                 </View>
-                <TextInput placeholder={'(555) 555-5678'}
-                  keyboardType={'numeric'}
-                  onChangeText={(mobile) => this.setState({
-                    mobile
-                  })}
+                <TextInput
+                  placeholder={"(555) 555-5678"}
+                  keyboardType={"numeric"}
+                  onChangeText={(mobile) =>
+                    this.setState({
+                      mobile,
+                    })
+                  }
                   value={this.state.mobile}
-                  style={[textIn.input, { width: wp('64%') }]} />
-
+                  style={[textIn.input, { width: wp("64%") }]}
+                />
               </View>
 
               <View
@@ -252,37 +295,49 @@ export default class SignUp1 extends React.Component {
                   alignItems: "flex-end",
                 }}
               >
-                {this.state.numVerified ?
+                {this.state.numVerified ? (
                   <LatoText
                     fontName="Lato-Regular"
                     fonSiz={17}
                     col="#2AA034"
                     text={"Verified"}
-                  /> :
+                  />
+                ) : (
                   <TouchableOpacity
                     style={{
                       paddingHorizontal: 20,
                       paddingVertical: 10,
-                      borderColor: '#C9C9C9',
+                      borderColor: "#C9C9C9",
                       borderWidth: 1,
-                      borderRadius: 5
+                      borderRadius: 5,
                     }}
                     onPress={async () => {
-                      var num =Math.floor(100000 + Math.random() * 900000)
-                      await this.setState({num: num.toString()})
-                      this.forceUpdate()
+                      var num = Math.floor(100000 + Math.random() * 900000);
+                      await this.setState({ num: num.toString() });
+                      this.forceUpdate();
 
-                      axios.get("https://sheltered-scrubland-52295.herokuapp.com/api/email/verification/"+this.state.email+"/"+num)
-                      .then(resp =>this.refs.modal3.open())
-                      .catch(err => console.log(err))
+                      axios
+                        .get(
+                          "https://sheltered-scrubland-52295.herokuapp.com/api/email/verification/" +
+                            this.state.email +
+                            "/" +
+                            num
+                        )
+                        .then((resp) => this.refs.modal3.open())
+                        .catch((err) => console.log(err));
 
-                      axios.get("https://sheltered-scrubland-52295.herokuapp.com/api/number/verification/"+"+"+this.state.mobile+"/"+num)
-                      .then(resp =>this.refs.modal3.open())
-                      .catch(err => console.log(err))
-                      
+                      axios
+                        .get(
+                          "https://sheltered-scrubland-52295.herokuapp.com/api/number/verification/" +
+                            "+" +
+                            this.state.mobile +
+                            "/" +
+                            num
+                        )
+                        .then((resp) => this.refs.modal3.open())
+                        .catch((err) => console.log(err));
                     }}
                   >
-
                     <LatoText
                       fontName="Lato-Regular"
                       fonSiz={17}
@@ -290,8 +345,7 @@ export default class SignUp1 extends React.Component {
                       text={"Verify"}
                     />
                   </TouchableOpacity>
-                }
-
+                )}
               </View>
               <View>
                 <View style={textIn.label}>
@@ -303,15 +357,17 @@ export default class SignUp1 extends React.Component {
                   />
                 </View>
                 <View>
-
-                  <TextInput placeholder={'00000'} style={textIn.input}
-                    onChangeText={(zipCode) => this.setState({
-                      zipCode
-                    })}
-                    value={this.state.zipCode} />
-
+                  <TextInput
+                    placeholder={"00000"}
+                    style={textIn.input}
+                    onChangeText={(zipCode) =>
+                      this.setState({
+                        zipCode,
+                      })
+                    }
+                    value={this.state.zipCode}
+                  />
                 </View>
-
               </View>
             </View>
           </View>
@@ -319,18 +375,20 @@ export default class SignUp1 extends React.Component {
             style={{
               justifyContent: "space-evenly",
 
-              paddingHorizontal: wp("10%")
+              paddingHorizontal: wp("10%"),
             }}
           >
             <TouchableOpacity
               style={btnStyles.basic}
-              onPress={() => this.props.navigation.navigate("ChoosePass", {
-                name: this.state.name,
-                email: this.state.email,
-                mobile: this.state.mobile,
-                zipCode: this.state.zipCode,
-                password: this.state.password,
-              })}
+              onPress={() =>
+                this.props.navigation.navigate("ChoosePass", {
+                  name: this.state.name,
+                  email: this.state.email,
+                  mobile: this.state.mobile,
+                  zipCode: this.state.zipCode,
+                  password: this.state.password,
+                })
+              }
             >
               <LatoText
                 fontName="Lato-Regular"
@@ -350,9 +408,7 @@ export default class SignUp1 extends React.Component {
                 text={"Already a member Sign In "}
               />
             </TouchableOpacity>
-
           </View>
-
         </ScrollView>
       </SafeAreaView>
     );
@@ -360,37 +416,36 @@ export default class SignUp1 extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
   wrapper: {
     paddingTop: 50,
-    flex: 1
+    flex: 1,
   },
 
   modal: {
     borderRadius: 10,
     padding: 20,
-    alignItems: 'center'
+    alignItems: "center",
   },
 
   modal2: {
     height: 230,
-    backgroundColor: "#3B5998"
+    backgroundColor: "#3B5998",
   },
 
   modal3: {
     height: 230,
-    width: 300
+    width: Dimensions.get("window").width - 100,
   },
 
   modal4: {
-    height: 300
+    height: 300,
   },
 
   btn: {
     margin: 10,
     backgroundColor: "#3B5998",
     color: "white",
-    padding: 10
+    padding: 10,
   },
 
   btnModal: {
@@ -399,22 +454,21 @@ const styles = StyleSheet.create({
     right: 0,
     width: 50,
     height: 50,
-    backgroundColor: "transparent"
+    backgroundColor: "transparent",
   },
 
   text: {
     color: "black",
-    fontSize: 22
+    fontSize: 22,
   },
   logo: {
     width: wp("30%"),
-    alignSelf: "center"
+    alignSelf: "center",
   },
   icon: {
     position: "absolute",
     right: 10,
-    paddingTop: 8
+    paddingTop: 8,
   },
-  myText: { fontSize: hp("5%") }
-
+  myText: { fontSize: hp("5%") },
 });
