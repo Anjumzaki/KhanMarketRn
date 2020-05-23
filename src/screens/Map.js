5356913f8f29b2b151357a9a7d5c4b30cd8c7ec7
aijazalishah <aijazalishah@live.com>
2020-05-23 23:31:48 +0500

    sd

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -32,7 +32,6 @@ class Map extends React.Component {
       console.log("ONFFOOO", info.coords.latitude);
       lat = info.coords.latitude;
       lng = info.coords.longitude;
-<<<<<<< HEAD
       this.setState({
         lat,
         lng,
@@ -65,53 +64,6 @@ class Map extends React.Component {
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 }
     );
     //   console.log("lat longgg", lat1,lng1)
-=======
-      this.setState(
-        {
-          lat,
-          lng,
-        },
-        () => {
-          fetch(
-            "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-              info.coords.latitude +
-              "," +
-              info.coords.longitude +
-              "&key=AIzaSyCYwrgArmp1NxJsU8LsgVKu5De5uCx57dI"
-          )
-            .then((response) => response.json())
-            .then((responseJson) => {
-              // console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[0].long_name))
-              //   console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[1].long_name))
-              //   console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[2].long_name))
-              //   console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[3].long_name))
-              //   console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[4].long_name))
-              //   console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[5].long_name));
-              // alert(JSON.stringify(responseJson.results[0].formatted_address))
-              this.setState({ completeLoc: responseJson });
-              this.props.locationAsync(
-                JSON.stringify(responseJson.results[0].formatted_address)
-              );
-            })
-            .catch((err) => console.log("err", err));
-
-          this.setState(
-            {
-              location: info,
-              lat: info.coords.latitude,
-              lng: info.coords.longitude,
-            }
-
-            // alert(JSON.stringify(info))
-          );
-        },
-        (error) => {
-          console.log(error);
-        },
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 }
-      );
-    });
->>>>>>> 167e1fc25fa03e8f639de779773670d2e5c4398c
   }
   render() {
     console.log(this.state);
@@ -164,11 +116,7 @@ class Map extends React.Component {
                   })
                   .catch(err => console.log(err))
               }}
-<<<<<<< HEAD
               // onPress={() => this.props.navigation.push("App")}
-=======
-              onPress={() => this.props.navigation.push("App")}
->>>>>>> 167e1fc25fa03e8f639de779773670d2e5c4398c
               style={[btnStyles.basic, { width: "80%", marginBottom: 100 }]}
             >
               <LatoText
