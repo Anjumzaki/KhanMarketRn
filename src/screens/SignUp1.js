93890dd3d322933b2fa03d4fc1dfd31fb976dca5
aijazalishah <aijazalishah@live.com>
2020-06-04 05:45:05 +0500

    sd

--- src/screens/SignUp1.js
+++ src/screens/SignUp1.js
@@ -513,13 +513,13 @@ export default class SignUp1 extends React.Component {
                     }}
                     onPress={async () => {
                       var num = Math.floor(100000 + Math.random() * 900000);
-                      await thissetState({ num: num.toString() });
+                      await this.setState({ num: num.toString() });
                       this.forceUpdate();
                       axios.get("https://lit-peak-13067.herokuapp.com/get/user/"+this.state.email.trim())
                       .then(resp => {
                         axios.get("https://lit-peak-13067.herokuapp.com/get/user/number/"+mainNumber)
                         .then(resp1 => {
-                        
+                          console.log("sddsdd", resp.data, resp1.data)
                             if(resp.data === null){
                               if(resp1.data === null){
                               var cd = "1"
@@ -547,12 +547,14 @@ export default class SignUp1 extends React.Component {
                               .then((resp) => this.refs.modal3.open())
                               .catch((err) => console.log("num err",err));
                             }else{
-                              this.setState({errMessage: "Email already exist."})
+                              this.setState({errMessage: "Phone Number already exist."})
                             }
                           }else{
-                            this.setState({errMessage: "Phone Number already exist."})
+                            this.setState({errMessage: "Email already exist."})
                           }
                       })
+                      .catch(err => console.log(err))
+                      .catch(err => console.log(err))
                     })
                     }} 
                   >
