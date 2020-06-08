9ed4f2172b06479061f91077851706747ebfe1c0
Anjumzaki <anjumzaki8@gmail.com>
2020-06-09 00:47:55 +0500

    name changed

--- android/app/src/main/java/com/nodegrocery/MainApplication.java
+++ android/app/src/main/java/com/nodegrocery/MainApplication.java
@@ -1,4 +1,4 @@
-package com.khanmarket;
+package com.nodegrocery;
 
 import android.app.Application;
 
@@ -8,7 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.khanmarket.generated.BasePackageList;
+import com.nodegrocery.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import 'react-native-get-random-values';
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('khanmarket', () => App);
+AppRegistry.registerComponent('NodeGrocery', () => App);
 
 if (Platform.OS === 'web') {
   const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('khanmarket', { rootTag });
+  AppRegistry.runApplication('NodeGrocery', { rootTag });
 }
